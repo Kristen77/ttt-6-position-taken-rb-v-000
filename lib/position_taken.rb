6ffcb2f9ab61890d1725ebeff63fd5e2fdd0a925
Kristen Robertson <kristen77@gmail.com>
2018-08-21 02:17:36 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,11 +3,8 @@ def position_taken?(board, index)
   
  if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
        return false
-  return false
 
-  
-elsif
-  board[index] == "X" || board[index] == "O"
+else
   return true 
 
   
